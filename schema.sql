4a0428d739fad8de06282387cd4687db0ae1484f
sanyabhatia28 <141518925+sanyabhatia28@users.noreply.github.com>
2024-12-04 21:41:51 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,23 +1,4 @@
---CREATE DATABASE expense_tracker;
---
---USE expense_tracker;
---
---CREATE TABLE users (
---    user_id INT AUTO_INCREMENT PRIMARY KEY,
---    username VARCHAR(50) NOT NULL,
---    password VARCHAR(100) NOT NULL,
---    email VARCHAR(100) NOT NULL
---);
---
---CREATE TABLE expenses (
---    expense_id INT AUTO_INCREMENT PRIMARY KEY,
---    user_id INT,
---    amount DECIMAL(10, 2),
---    category VARCHAR(50),
---    date DATE,
---    description TEXT,
---    FOREIGN KEY (user_id) REFERENCES users(user_id)
---);
+
 CREATE DATABASE IF NOT EXISTS expense_tracker;
 
 USE expense_tracker;
